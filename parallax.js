ce252790c978354bec21ef43ae4af3d5961d120c
manoj <manoj kumar>
2019-07-16 15:39:08 +0530

    init

---
+++ parallax.js
@@ -0,0 +1,42 @@
+var image = document.getElementsByClassName('11400');
+new simpleParallax(image, {
+    orientation: 'right',
+    overflow: 'true',
+    scale: '3',
+    delay: '4'
+});
+var image = document.getElementsByClassName('bt');
+new simpleParallax(image, {
+    orientation: 'right',
+    overflow: 'true',
+    scale: '3',
+    delay: '4'
+});
+
+//end of section 1
+
+var image = document.getElementsByClassName('bt2r');
+new simpleParallax(image, {
+    orientation: 'up-right',
+    overflow: 'true',
+    scale: '2',
+    delay: '4'
+});
+
+// end of section 2
+
+var image = document.getElementsByClassName('bt3r');
+new simpleParallax(image, {
+    orientation: 'up-right',
+    overflow: 'true',
+    scale: '2',
+    delay: '4'
+});
+
+var image = document.getElementsByClassName('bt3l');
+new simpleParallax(image, {
+    orientation: 'up-left',
+    overflow: 'true',
+    scale: '2.4',
+    delay: '2'
+});
---
+++ tip.js
@@ -0,0 +1,42 @@
+tippy('.s1t1', {
+    content: '<img src="images/tweeter.jpg" width="100%" height="auto">',
+    theme: 'light-border',
+    arrow: 'true',
+  })
+tippy('.s1t2', {
+    content: '<img src="images/LED Display.jpg" width="100%" height="auto">',
+    theme: 'light-border',
+    arrow: 'true',
+})
+
+tippy('.s1t3', {
+    content: '<img src="images/specs.jpg" width="100%" height="auto">',
+    arrow: 'true',
+    theme: 'light',
+})
+//end section 1
+tippy('.s2t1', {
+    content: '<img src="images/FM Ant.jpg" width="100%" height="auto">',
+    arrow: 'true',
+    theme: 'light',
+  })
+
+tippy('.s2t2', {
+    content: '<img src="images/Power switch.JPG" width="100%" height="auto">',
+    arrow: 'true',
+    theme: 'light',
+})
+
+tippy('.s2t3', {
+    content: '<img src="images/Right input.JPG" width="100%" height="auto">',
+    arrow: 'true',
+    theme: 'light',
+    placement: 'right',
+})
+//end section 1
+tippy('.s3t1', {
+    content: '<img src="images/bass.JPG" width="100%" height="auto">',
+    arrow: 'true',
+    theme: 'light',
+    placement: 'bottom',
+})
